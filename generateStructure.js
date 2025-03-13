aa7b3bcb896bd68e090e483452328eb05ab19125
Rakesh Biswas <rbiswas01999@gmail.com>
2025-03-13 08:19:06 +0600

    generateStructure.ts -file added

--- generateStructure.js
+++ generateStructure.js
@@ -1,31 +1,10 @@
-// // generateStructure.ts
-// // import fs from "fs";
-// // import path from "path";
-// import * as fs from 'fs';
-// import * as path from 'path';
-// function getStructure(dir: string, depth = 0): string {
-//   const files = fs.readdirSync(dir);
-//   let structure = "";
-//   files.forEach((file) => {
-//     const fullPath = path.join(dir, file);
-//     const isDirectory = fs.statSync(fullPath).isDirectory();
-//     structure += "  ".repeat(depth) + "|-- " + file + "\n";
-//     if (isDirectory) {
-//       structure += getStructure(fullPath, depth + 1);
-//     }
-//   });
-//   return structure;
-// }
-// // Run and save the output
-// const projectStructure = getStructure(process.cwd());
-// fs.writeFileSync("project-structure.txt", projectStructure);
-// console.log("✅ Project structure saved to project-structure.txt");
-// // # Compile
-// // npx tsc generateStructure.ts
-// // # Run
-// // node generateStructure.js
-var fs = require('fs');
-var path = require('path');
+"use strict";
+// generateStructure.ts
+Object.defineProperty(exports, "__esModule", { value: true });
+// import fs from "fs";
+// import path from "path";
+var fs = require("fs");
+var path = require("path");
 function getStructure(dir, depth) {
     if (depth === void 0) { depth = 0; }
     var files = fs.readdirSync(dir);
@@ -41,6 +20,6 @@ function getStructure(dir, depth) {
     return structure;
 }
 // Run and save the output
-var projectStructure = getStructure(__dirname);
+var projectStructure = getStructure(process.cwd());
 fs.writeFileSync("project-structure.txt", projectStructure);
 console.log("✅ Project structure saved to project-structure.txt");
